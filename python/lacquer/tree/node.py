32b5dee8a5468c88e1682063f4a6d41190b37013
Brian Van Klaveren <bvan@slac.stanford.edu>
2016-04-02 20:01:29 -0700

    Print if values are False or 0

--- python/lacquer/tree/node.py
+++ python/lacquer/tree/node.py
@@ -13,11 +13,11 @@ class Node(object):
         visitor.visit_node(self, context)
 
     def __str__(self):
-        return str({k: v for k, v in self.__dict__.items() if v})
+        return str({k: v for k, v in self.__dict__.items() if v is not None})
 
     def __repr__(self):
         clz = self.__class__
-        argspec = [x for x in getargspec(clz.__init__).args[3:] if self.__dict__[x]]
+        argspec = [x for x in getargspec(clz.__init__).args[3:] if self.__dict__[x] is not None]
         args = ", ".join(["=".join((arg, repr(getattr(self, arg)))) for arg in argspec])
         return "{name}({args})".format(name=clz.__name__, args=args)
 
