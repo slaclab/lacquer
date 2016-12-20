e7b5d97993ad0b649cae617a1e2575b3f5b092dc
Brian Van Klaveren <bvan@slac.stanford.edu>
2016-12-20 11:46:45 -0800

    Add prototype json formatter

---
+++ python/lacquer/formatter/json.py
@@ -0,0 +1,22 @@
+from collections import OrderedDict
+from lacquer.tree import QualifiedName, QuerySpecification
+from lacquer.tree.node import Node
+from json import JSONEncoder
+
+
+class NodeEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, QuerySpecification):
+            keys = ("select", "from_", "where", "group_by", "having", "order_by", "limit")
+            ret = OrderedDict([(key, getattr(obj, key)) for key in keys if getattr(obj, key)])
+            return ret
+
+        if isinstance(obj, Node):
+            keys = [key for key in obj.__dict__.keys() if key[0] != '_' and key not in ('line', 'pos')]
+            ret = {key: getattr(obj, key) for key in keys if getattr(obj, key)}
+            return ret
+
+        if isinstance(obj, QualifiedName):
+            return obj.parts
+
+        return JSONEncoder.default(self, obj)
